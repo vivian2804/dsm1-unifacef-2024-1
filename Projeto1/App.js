155fea13811ede1a733fa523c2bf4eb19c92c9b4
Fausto G. Cintra <professor@faustocintra.com.br>
2024-02-27 20:39:19 -0300

    (27/02) Primeiro projeto

---
+++ Projeto1/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Olá, mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 30
+  }
+});

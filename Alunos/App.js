5c0a309214509c54c9b829926dbe5832570d923b
Fausto G. Cintra <professor@faustocintra.com.br>
2024-03-12 20:25:44 -0300

    (12/03) Correção atividade component Student

--- Alunos/App.js
+++ Alunos/App.js
@@ -1,29 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Card from './src/Card'
-import Student from './src/Student'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Olá, mundo!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Card title="Uni-FACEF">Centro Universitário Municipal de Franca</Card>
-      <Student name="Adalberto Alves" grade1={6.5} grade2={7.3} /> 
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontWeight: 'bold',
-    fontSize: 30
-  }
 });
---
+++ Projeto1/src/Student/styles.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#FFFFE0',
+    border: '2px solid #FF4500',
+    padding: 16
+  },
+  title: {
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    marginBottom: 5
+  },
+  titleText: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  horizontal: {
+    flexDirection: 'row'
+  },
+  textBlue: {
+    color: '#0000CD'
+  }
+})
+
+export default styles

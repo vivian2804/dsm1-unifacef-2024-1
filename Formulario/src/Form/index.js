7fafda232f86184017131599f2886aed64622cb0
Fausto G. Cintra <professor@faustocintra.com.br>
2024-04-09 20:04:14 -0300

    (09/04) Término do projeto Formulario

--- Formulario/src/Form/index.js
+++ Formulario/src/Form/index.js
@@ -7,14 +7,16 @@ export default function Form() {
   const [state, setState] = React.useState({
     base: '',
     altura: '',
-    tipo: ''
+    tipo: '',
+    area: ''
   })
   // Criando as variáveis de estado somente-leitura
   // usando desestruturação
   const {
     base,
     altura,
-    tipo
+    tipo,
+    area
   } = state
 
   // Funções de manipulação de eventos usando o formato arrow function
@@ -27,8 +29,33 @@ export default function Form() {
   }
 
   function handleButtonPress() {
-  
+    let result
+    switch(tipo.toUpperCase()) {
+      case 'R':     // Retângulo
+        result = Number(base) * Number(altura)
+        break
+      case 'T':     // Triângulo
+        result = Number(base) * Number(altura) / 2
+        break
+      case 'E':     // Elipse/círculo
+        result = (Number(base) / 2) * (Number(altura) / 2) * Math.PI
+        break
+      default:      // Forma inválida ou desconhecida
+        result = '[TIPO INVÁLIDO]'
+    }
+    // Atualiza o estado da área com o resultado encontrado
+    setState({...state, area: result})
   }
+
+  /*
+    Propriedades do TextInput
+    inputMode: determina o tipo de entrada aceita pelo TextInput
+      (text, decimal, numeric, email, tel, etc. - ver documentação
+      do React Native para ver todos os valores possíveis)
+    keyboardType: determina o tipo de teclado que aparecerá quando
+      o TextInput estiver focado (default, number-pad, decimal-pad,
+      numeric, etc. - ver documentação para todos os valores possíveis)
+  */
 
   return (
     <View>
@@ -38,6 +65,8 @@ export default function Form() {
         <TextInput 
           style={styles.textInput}
           onChangeText={handleBaseChangeText}
+          inputMode="decimal"
+          keyboardType="decimal-pad"
         />
       </View>
       <View style={styles.horizontal}>
@@ -45,6 +74,8 @@ export default function Form() {
         <TextInput 
           style={styles.textInput}
           onChangeText={handleAlturaChangeText}
+          inputMode="decimal"
+          keyboardType="decimal-pad"
         />
       </View>
       <View style={styles.horizontal}>
@@ -52,14 +83,15 @@ export default function Form() {
         <TextInput 
           style={styles.textInput}
           onChangeText={handleTipoChangeText}
+          maxLength={1}
+          autoCapitalize="characters"
         />
       </View>
       <View style={styles.horizontal}>
         <Button title="Calcular" onPress={handleButtonPress} />
       </View>
       <View style={styles.horizontal}>
-        <Text>Área: </Text>
-        <Text>Estado: {JSON.stringify(state)}</Text>
+        <Text>Área: {area}</Text>
       </View>
     </View>
   )
